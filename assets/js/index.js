34eea364995ba53fdd4d7fb69499666e338ddc50
poonsolai <poonsolai05@gmail.com>
2025-09-24 05:49:26 +0530

    update the code

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,6 +26,8 @@ function btnchange(val,vals,dish,rate){
 }
 
 let itemcart = document.getElementById("item-cart") ;
+let total = document.getElementById("total") ;
+let num = 1;
 function addcart(dish,rate){
     let parentdiv = document.createElement("div");
     let div = document.createElement("div");
@@ -41,14 +43,64 @@ function addcart(dish,rate){
     p1.classList = "dcount";
     p2.classList = "rate";
     p3.classList = "addrate";
+    p3.id = `${idcreate(dish)}`
     itemcart.prepend(parentdiv);
     parentdiv.appendChild(div);
     div.appendChild(dishname).innerHTML = dish;
     div.appendChild(detaialdiv);
     parentdiv.appendChild(closediv);
     detaialdiv.appendChild(p1).innerHTML = "1x";
-    detaialdiv.appendChild(p2).innerHTML = rate;
-    detaialdiv.appendChild(p3).innerHTML = rate;
+    detaialdiv.appendChild(p2).innerHTML = `@ $${rate}`;
+    detaialdiv.appendChild(p3).innerHTML = `$${rate}`;
+    total.classList = "total-order"
+    calrate(rate)
+    // console.log(num)
+    // adddishcount(`addrate${num}`)
+    // num++
+    console.log(p3)
+}
+
+function idcreate(dname){
+    if(dname=='Pistachio baklva'){
+        return "baklva"
+    }
+    else if(dname=='Salted Caramel Brownie'){
+        return 'brownie'
+    }
+    else if(dname=='Red Velvet Cake'){
+        return 'cake'
+    }
+    else if(dname=='Vanilla Bean Crème Brûlée'){
+        return 'brulee'
+    }
+    else if(dname=='Macaron Mix of Five'){
+        return 'macaron'
+    }
+    else if(dname=='Lemon Meringue Pie'){
+        return 'pie'
+    }
+    else if(dname=='Vanilla Panna Cotta'){
+        return 'cotta'
+    }
+    else if(dname=='Classic Tiramisu'){
+        return 'tiramisu'
+    }
+    else if(dname=='Waffle with Berries'){
+        return 'waffle'
+    }
+}
+
+let total_rate =Number("");
+function calrate(rate){
+    let num = parseFloat(rate);
+    total_rate +=num;
+    let showrate = document.getElementById("rate")
+    showrate.innerHTML = `$${total_rate.toFixed(2)}`
+}
+let amount = 111111;
+function adddishcount(val){
+    let addrate = document.getElementById(val)
+    addrate.innerHTML = amount
 }
 
 function showemptyimgandtext(){
